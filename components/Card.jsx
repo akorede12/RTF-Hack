a669731d599de1029397508f63630e13408929c9
Siddharth Manjul <manjulsiddharth09@gmail.com>
2024-02-03 23:37:12 +0530

    Final Touches

--- components/Card.jsx
+++ components/Card.jsx
@@ -9,23 +9,23 @@ const Card = ({ appartment }) => {
     <div className="shadow-md w-96 text-xl pb-5 rounded-b-2xl mb-20">
       <Link href={'/room/' + appartment.id}>
         <ImageSlider images={appartment.images} />
+        <div className="px-4">
+          <div className="flex justify-between items-start mt-2">
+            <p className="font-semibold capitalize text-[15px]">{appartment.name}</p>
+            <p className="flex justify-start items-center space-x-2 text-sm">
+              <FaStar />
+              <span>New</span>
+            </p>
+          </div>
+          <div className="flex justify-between items-center text-sm">
+            <p className="text-gray-700">{formatDate(appartment.timestamp)}</p>
+            <b className="flex justify-start items-center space-x-1 font-semibold">
+              <FaEthereum />
+              <span>{appartment.price} Night</span>
+            </b>
+          </div>
+        </div>
       </Link>
-      <div className="px-4">
-        <div className="flex justify-between items-start mt-2">
-          <p className="font-semibold capitalize text-[15px]">{appartment.name}</p>
-          <p className="flex justify-start items-center space-x-2 text-sm">
-            <FaStar />
-            <span>New</span>
-          </p>
-        </div>
-        <div className="flex justify-between items-center text-sm">
-          <p className="text-gray-700">{formatDate(appartment.timestamp)}</p>
-          <b className="flex justify-start items-center space-x-1 font-semibold">
-            <FaEthereum />
-            <span>{appartment.price} Night</span>
-          </b>
-        </div>
-      </div>
     </div>
   )
 }
--- components/ImageSlider.jsx
+++ components/ImageSlider.jsx
@@ -10,10 +10,10 @@ const ImageSlider = ({ images }) => {
     <Swiper
       spaceBetween={30}
       centeredSlides={true}
-      autoplay={{
-        delay: 2500,
-        disableOnInteraction: false,
-      }}
+      // autoplay={{
+      //   delay: 2500,
+      //   disableOnInteraction: false,
+      // }}
       pagination={{
         clickable: true,
       }}
